af3b5b0069a12955c64161ea6e2ae2d75e1b00d0
Kartik Balasubramaniam <saykartik@gmail.com>
2019-11-25 17:38:11 -0500

    Added new scripts

--- scripts/exp_case_inception2.sh
+++ scripts/exp_case_inception2.sh
@@ -1,12 +1,12 @@
 #! /bin/sh
 
-NET_FILE=nets/inception_v3.json
-OUT_FILE=results/case_inception
+NET_FILE=nets/inception_v32.json
+OUT_FILE=results/case_inception2
 
 PPP_COMP=0.62
 PPP_COMM=0.72
 
-echo "Inception v3 experiment\n$(date)\n" > $OUT_FILE
+echo "Inception v32 experiment\n$(date)\n" > $OUT_FILE
 
 # Summary, too long.
 # ./paleo.sh summary $NET_FILE
@@ -15,7 +15,7 @@ paleo.sh simulate $NET_FILE \
     --batch_size=64 \
     --network_name=ethernet\
     --device_name=K40 \
-    --num_workers=1,2,4,8 \
+    --num_workers=1\
     --ppp_comp=$PPP_COMP \
     --ppp_comm=$PPP_COMM \
     --scaling=strong >> $OUT_FILE
